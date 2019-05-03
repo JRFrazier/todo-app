1797b26fc57fa3e733a46ed87a1dc69be78f9320
JR <jfrazier@tiggee.com>
2019-05-03 13:32:07 -0400

    fist batch

---
+++ app.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import db from './db/db';
+import bodyParser from 'body-parser'
+const PORT = 5000;
+
+// Set up the express app
+const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
+});
+
+app.post('/api/v1/todos', (req, res) => {
+  console.log(req.body)
+  if(!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  } else if(!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+ const todo = {
+   id: db.length + 1,
+   title: req.body.title,
+   description: req.body.description
+ }
+
+ db.push(todo);
+
+ return res.status(201).send({
+   success: 'true',
+   message: 'todo added successfully',
+   todo
+ })
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
